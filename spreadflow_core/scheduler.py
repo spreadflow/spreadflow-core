30a76ff6e08d5a5da87f578b3ec5f4d56249d4d5
Lorenz Schori <lo@znerol.ch>
2016-01-18 17:37:05 +0100

    Scheduler: Add log messages

--- spreadflow_core/scheduler.py
+++ spreadflow_core/scheduler.py
@@ -59,29 +59,45 @@ class Scheduler(object):
         if reactor == None:
             from twisted.internet import reactor
 
+        self.log.info('Starting scheduler')
         self._queue_task = task.cooperate(self._queue)
         self._queue_done = self._queue_task.whenDone()
         self._enqueuer = collections.defaultdict(lambda: self._enqueue)
 
+        self.log.debug('Applying {decogen_len} port decorator generators', decogen=self.flowmap.decorators, decogen_len=len(self.flowmap.decorators))
         for decogen in self.flowmap.decorators:
             for proc, decorator in decogen(self, reactor):
                 self._enqueuer[proc] = decorator(self._enqueuer[proc])
+        self.log.debug('Applied {decogen_len} port decorator generators', decogen=self.flowmap.decorators, decogen_len=len(self.flowmap.decorators))
 
         flowgraph = self.flowmap.graph()
 
         is_attachable = lambda p: hasattr(p, 'attach') and callable(p.attach)
         procs = graph.vertices(graph.contract(flowgraph, is_attachable))
+        self.log.debug('Attaching {procs_len} sources and services', procs=procs, procs_len=len(procs))
         yield defer.DeferredList([defer.maybeDeferred(p.attach, self, reactor) for p in procs], fireOnOneErrback=True)
+        self.log.debug('Attached {procs_len} sources and services', procs=procs, procs_len=len(procs))
 
         is_startable = lambda p: hasattr(p, 'start') and callable(p.start)
-        plan = toposort(graph.contract(flowgraph, is_startable))
-        for proc_set in plan:
+        plan = list(toposort(graph.contract(flowgraph, is_startable)))
+        for batch_num, proc_set in enumerate(plan, 1):
+            logkwds = {
+                'procs': proc_set,
+                'procs_len': len(proc_set),
+                'batch_num': batch_num,
+                'batch_len': len(plan)
+            }
+            self.log.debug('Starting {procs_len} sources and services ({batch_num}/{batch_len})', **logkwds)
             yield defer.DeferredList([defer.maybeDeferred(p.start) for p in proc_set], fireOnOneErrback=True)
+            self.log.debug('Started {procs_len} sources and services ({batch_num}/{batch_len})', **logkwds)
+
+        self.log.info('Started scheduler')
 
         yield self._done
 
     def stop(self, reason):
         if not self._stopped:
+            self.log.info('Stopping scheduler', reason=reason)
             self._stopped = True
             return self._done.callback(reason)
         else:
@@ -93,6 +109,7 @@ class Scheduler(object):
         self._stopped = True
 
         # Cancel all pending jobs.
+        self.log.debug('Cancel {pending_len} pending jobs', pending=self._pending, pending_len=len(self._pending))
         _trapcancel = lambda f: f.trap(defer.CancelledError)
         _logfail = lambda f, port, handler, item, send: self.log.failure('Failed to cancel job', f, port=port, item=item, handler=handler, send=send)
         for deferred_job, (port, handler, item, send) in self._pending.items():
@@ -104,23 +121,35 @@ class Scheduler(object):
         # Clear the backlog and wait for queue termination.
         self._queue.clear()
         self._queue.stopempty = True
+        self.log.debug('Stopping queue')
         yield self._queue_done
         self._pending.clear()
+        self.log.debug('Stopped queue')
 
         flowgraph = self.flowmap.graph()
 
         # Join sources and services.
         is_joinable = lambda p: hasattr(p, 'join') and callable(p.join)
-        plan = toposort(graph.reverse(graph.contract(flowgraph, is_joinable)))
-        for proc_set in plan:
+        plan = list(toposort(graph.reverse(graph.contract(flowgraph, is_joinable))))
+        for batch_num, proc_set in enumerate(plan, 1):
             job_list = []
             for proc in proc_set:
                 _logfail = lambda f, proc=proc: self.log.failure('Failed to join proc {proc}', f, proc=proc)
                 join_job = defer.maybeDeferred(proc.join).addErrback(_logfail)
                 job_list.append(join_job)
 
-            yield defer.DeferredList(job_list)
+            logkwds = {
+                'procs': proc_set,
+                'procs_len': len(proc_set),
+                'batch_num': batch_num,
+                'batch_len': len(plan)
+            }
 
+            self.log.debug('Joining {procs_len} sources and services ({batch_num}/{batch_len})', **logkwds)
+            yield defer.DeferredList(job_list)
+            self.log.debug('Joined {procs_len} sources and services ({batch_num}/{batch_len})', **logkwds)
+
+        # Detach sources and services.
         is_detachable = lambda p: hasattr(p, 'detach') and callable(p.detach)
         procs = graph.vertices(graph.contract(flowgraph, is_detachable))
         job_list = []
@@ -128,8 +157,13 @@ class Scheduler(object):
             _logfail = lambda f, proc=proc: self.log.failure('Failed to detach proc {proc}', f, proc=proc)
             detach_job = defer.maybeDeferred(proc.detach).addErrback(_logfail)
             job_list.append(detach_job)
+
+        self.log.debug('Detaching {procs_len} sources and services', procs=procs, procs_len=len(procs))
         yield defer.DeferredList(job_list)
+        self.log.debug('Detached {procs_len} sources and services', procs=procs, procs_len=len(procs))
 
         self._enqueuer = None
         self._queue_done = None
         self._queue_task = None
+
+        self.log.info('Stopped scheduler')
