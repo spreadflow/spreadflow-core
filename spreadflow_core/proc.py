9937b33b4609947c9f894a2eb1fd0e5b18c068da
Lorenz Schori <lo@znerol.ch>
2016-05-27 15:19:56 +0200

    Remove unused imports

--- spreadflow_core/proc.py
+++ spreadflow_core/proc.py
@@ -7,7 +7,7 @@ import copy
 from twisted.internet import defer, task
 from twisted.logger import Logger, LogLevel
 
-from spreadflow_core.component import ComponentBase, PortCollection, RegisteredComponent
+from spreadflow_core.component import ComponentBase
 
 
 class SyntheticSource(object):
