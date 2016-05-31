30b37814960dc8db288f2d09d535c97f1bbf228d
Lorenz Schori <lo@znerol.ch>
2016-05-31 11:37:18 +0200

    Remove cruft from config moule

--- spreadflow_core/config.py
+++ spreadflow_core/config.py
@@ -8,14 +8,8 @@ from spreadflow_core.dsl import Context
 def config_eval(path):
     module_name = 'spreadflow_core._conf{:X}'.format(hash(path))
 
-    # Evaluate config script.
-    components = []
-
     with Context(path) as ctx:
         with open(path, 'r') as f:
-            result = imp.load_source(module_name, path, f)
+            imp.load_source(module_name, path, f)
 
-    # Cleanup script environment.
-    import sys
-    sys.modules.pop('spreadflow_core.script')
     return ctx.tokens
